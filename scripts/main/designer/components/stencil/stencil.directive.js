d6791c58f1f3d11a5278665cd0386599912eb43b
Yevheniia Velychko <yevheniia.velychko@synapse.com>
2018-10-24 16:51:21 +0300

    Spencil component done

---
+++ scripts/main/designer/components/stencil/stencil.directive.js
@@ -0,0 +1,30 @@
+angular.module('designer.module')
+    .directive('myStencil', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'main/designer/components/stencil/stencil.template.html',
+
+            scope: {
+                components: '=',
+                onSelect: '&',
+            },
+
+            link: function ($scope) {
+                $scope.selectedComponent = null;
+
+                $scope.addSelectedComponent = function () {
+                    console.log($scope.selectedComponent);
+
+                    $scope.onSelect({component: $scope.selectedComponent});
+                };
+
+                $scope.selectComponent = function (component) {
+                    $scope.selectedComponent = component;
+                };
+
+                $scope.isComponentSelected = function (component) {
+                    return component === $scope.selectedComponent;
+                };
+            }
+        };
+    });
