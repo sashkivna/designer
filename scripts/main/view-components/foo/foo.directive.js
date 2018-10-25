fc5ace079c1a245386e17c091d98845b8ba742d1
Yevheniia Velychko <yevheniia.velychko@synapse.com>
2018-10-25 11:47:44 +0300

    Applied changes to styles

--- scripts/main/view-components/foo/foo.directive.js
+++ scripts/main/view-components/foo/foo.directive.js
@@ -2,6 +2,6 @@ angular.module('designer.module')
     .directive('fooComponent', function () {
         return {
             restrict: 'E',
-            templateUrl: 'main/view-components/type/type.template.html',
+            templateUrl: 'main/view-components/foo/foo.template.html',
         };
     });
