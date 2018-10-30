3b5935df8d6c6b6386a9db41cdb10754857328a8
Yevheniia Velychko <yevheniia.velychko@synapse.com>
2018-10-30 17:58:23 +0200

    applied comments

--- scripts/main/designer/components/inspector/inspector-component/fields/number/inspector-number-component.directive.js
+++ scripts/main/designer/components/inspector/inspector-component/fields/number/inspector-number-component.directive.js
@@ -2,6 +2,6 @@ angular.module('designer.module')
     .directive('numberField', function () {
         return {
             restrict: 'E',
-            templateUrl: 'main/designer/components/inspector/inspector-component/Fields/Number/inspector-number-component.template.html'
+            templateUrl: 'main/designer/components/inspector/inspector-component/fields/number/inspector-number-component.template.html'
         };
     });
--- scripts/main/designer/components/inspector/inspector-component/fields/text/inspector-text-component.directive.js
+++ scripts/main/designer/components/inspector/inspector-component/fields/text/inspector-text-component.directive.js
@@ -2,6 +2,6 @@ angular.module('designer.module')
     .directive('textField', function () {
         return {
             restrict: 'E',
-            templateUrl: 'main/designer/components/inspector/inspector-component/Fields/Text/inspector-text-component.template.html'
+            templateUrl: 'main/designer/components/inspector/inspector-component/fields/text/inspector-text-component.template.html'
         };
     });
